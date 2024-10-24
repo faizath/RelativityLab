561fd06c03186cc6d59000be623980131d2320e9
faizathr <121447622+faizathr@users.noreply.github.com>
2024-10-24 20:31:57 +0700

    bulk init

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+
+let comingsoon = () => {alert("Coming Soon!")};
+q("#playground").onclick = () => {
+    window.location.href = "lab.html";
+};
+q("#exercise").onclick = comingsoon;
+q("#login").onclick = comingsoon;
+q("#explore").onclick = comingsoon;
